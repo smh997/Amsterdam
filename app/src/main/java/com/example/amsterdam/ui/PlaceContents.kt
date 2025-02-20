3626253ca9cf33af79eb9eb8d00c0e91a8e066b9
smh10 <smh10.mh@gmail.com>
2025-02-20 13:54:47 -0500

    GoogleMap url of each place can be opened now. PlaceContents is more organized by implementing PlaceContentsDetails.

--- app/src/main/java/com/example/amsterdam/ui/PlaceContents.kt
+++ app/src/main/java/com/example/amsterdam/ui/PlaceContents.kt
@@ -1,8 +1,11 @@
 package com.example.amsterdam.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,12 +28,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.amsterdam.R
+import com.example.amsterdam.data.Place
+import com.example.amsterdam.data.PlaceType
 import com.example.amsterdam.data.local.LocalPlacesDataProvider
 import com.example.compose.AmsterdamTheme
 
@@ -52,96 +60,140 @@ fun PlaceContents(
             isFullScreen = true,
             onBackButtonClicked = onPlaceScreenBackPressed
         )
-
-        Column(
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .verticalScroll(rememberScrollState())
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-//                .background(color = MaterialTheme.colorScheme.)
-            ) {
-                Image(
-                    painter = painterResource(id = amsterdamUiState.currentSelectedPlace.imageRes),
-                    contentDescription = stringResource(amsterdamUiState.currentSelectedPlace.name),
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 16.dp)
-                        .clip(RoundedCornerShape(8.dp))
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = MaterialTheme.colorScheme.primaryContainer),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Icon(
-                    painter = painterResource(amsterdamUiState.currentPlaceType.iconRes),
-                    contentDescription = null,
-                    Modifier.size(40.dp),
-                    tint = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-                Spacer(Modifier.width(8.dp))
-                Text(
-                    stringResource(amsterdamUiState.currentSelectedPlace.name),
-                    style = MaterialTheme.typography.titleMedium,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.LocationOn,
-                    contentDescription = null,
-                    Modifier.size(20.dp),
-                    tint = MaterialTheme.colorScheme.primary
-                )
-                Spacer(Modifier.width(8.dp))
-                Text(
-                    stringResource(amsterdamUiState.currentSelectedPlace.address),
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.primary
-                )
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.map_24),
-                    contentDescription = null,
-                    Modifier.size(20.dp),
-                    tint = MaterialTheme.colorScheme.tertiary
-                )
-                Spacer(Modifier.width(8.dp))
-                Text(
-                    stringResource(R.string.googleMaps),
-                    style = MaterialTheme.typography.bodyMedium,
-                    fontStyle = FontStyle.Italic,
-                    color = MaterialTheme.colorScheme.tertiary
-                )
-            }
-
-            Text(
-                stringResource(amsterdamUiState.currentSelectedPlace.description),
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
-
+        PlaceContentsDetails(place = amsterdamUiState.currentSelectedPlace)
     }
+}
 
+@Composable
+fun PlaceContentsDetails(
+    place: Place,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        PlacePicture(
+            placeImageRes = place.imageRes,
+            contentDescription = stringResource(place.name)
+        )
+
+        PlaceNameFrame(
+            placeNameRes = place.name,
+            placeType = place.placeType
+        )
+
+        PlaceAddress(placeAddressRes = place.address)
+
+        PlaceGoogleMap(placeGoogleMapRes = place.googleMapsUrl)
+
+        // Place Description
+        Text(
+            stringResource(place.description),
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Composable
+fun PlacePicture(placeImageRes: Int, contentDescription: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = placeImageRes),
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+                .clip(RoundedCornerShape(8.dp))
+        )
+    }
+}
+
+@Composable
+fun PlaceNameFrame(placeNameRes: Int, placeType: PlaceType, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.primaryContainer),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(placeType.iconRes),
+            contentDescription = null,
+            Modifier.size(40.dp),
+            tint = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+        Spacer(Modifier.width(8.dp))
+        Text(
+            stringResource(placeNameRes),
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    }
+}
+
+@Composable
+fun PlaceAddress(placeAddressRes: Int, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.LocationOn,
+            contentDescription = null,
+            Modifier.size(20.dp),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Spacer(Modifier.width(8.dp))
+        Text(
+            stringResource(placeAddressRes),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Composable
+fun PlaceGoogleMap(placeGoogleMapRes: Int, modifier: Modifier= Modifier) {
+    val context = LocalContext.current
+    val url: String = stringResource(placeGoogleMapRes)
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(url)
+                )
+                context.startActivity(intent)
+            },
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.map_24),
+            contentDescription = null,
+            Modifier.size(20.dp),
+            tint = MaterialTheme.colorScheme.tertiary
+        )
+        Spacer(Modifier.width(8.dp))
+        Text(
+            stringResource(R.string.googleMaps),
+            style = MaterialTheme.typography.bodyMedium,
+            fontStyle = FontStyle.Italic,
+            color = MaterialTheme.colorScheme.tertiary,
+            textDecoration = TextDecoration.Underline,
+        )
+    }
 }
 
 
