0e0d0e3431c40b640dd9bc951b21ba6d976b5c9e
smh10 <smh10.mh@gmail.com>
2025-02-20 12:46:44 -0500

    App Typography is implemented.

--- app/src/main/java/com/example/amsterdam/ui/theme/Type.kt
+++ app/src/main/java/com/example/amsterdam/ui/theme/Type.kt
@@ -5,12 +5,29 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import com.example.amsterdam.R
 
 val AmsterdamFont = FontFamily(
     Font(R.font.amsterdam, FontWeight.Normal)
 )
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val PoppinsFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Poppins"),
+        fontProvider = provider,
+    )
+)
+val baseline = Typography()
+
 
 val Typography = Typography(
     displayLarge = TextStyle(
@@ -53,39 +70,47 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
+//    titleSmall = baseline.titleSmall.copy(fontFamily = PoppinsFontFamily),
     titleSmall = TextStyle(
         fontFamily = AmsterdamFont,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
-    bodyLarge = TextStyle(
-        fontFamily = AmsterdamFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = AmsterdamFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
-    ),
-    bodySmall = TextStyle(
-        fontFamily = AmsterdamFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    ),
-    labelLarge = TextStyle(
-        fontFamily = AmsterdamFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
-    ),
-    labelMedium = TextStyle(
-        fontFamily = AmsterdamFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = AmsterdamFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 11.sp
-    )
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = PoppinsFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = PoppinsFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = PoppinsFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = PoppinsFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = PoppinsFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = PoppinsFontFamily)
+//    bodyLarge = TextStyle(
+//        fontFamily = AmsterdamFont,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp
+//    ),
+//
+//    bodyMedium = TextStyle(
+//        fontFamily = AmsterdamFont,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 14.sp
+//    ),
+//    bodySmall = TextStyle(
+//        fontFamily = AmsterdamFont,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 12.sp
+//    ),
+//    labelLarge = TextStyle(
+//        fontFamily = AmsterdamFont,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 14.sp
+//    ),
+//    labelMedium = TextStyle(
+//        fontFamily = AmsterdamFont,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 12.sp
+//    ),
+//    labelSmall = TextStyle(
+//        fontFamily = AmsterdamFont,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 11.sp
+//    )
 )
