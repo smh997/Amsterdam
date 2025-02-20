00394109bfa559b5fe6ad4aa2875533abaf46970
smh10 <smh10.mh@gmail.com>
2025-02-20 13:05:37 -0500

    HomeScreen is implemented (compact mode).

--- app/src/main/java/com/example/amsterdam/ui/AmsterdamApp.kt
+++ app/src/main/java/com/example/amsterdam/ui/AmsterdamApp.kt
@@ -1,13 +1,38 @@
 package com.example.amsterdam.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.amsterdam.data.Place
+import com.example.amsterdam.data.PlaceType
 import com.example.compose.AmsterdamTheme
 
 @Composable
-fun AmsterdamApp(modifier: Modifier = Modifier) {
-    HomeScreen(modifier = modifier)
+fun AmsterdamApp(
+    modifier: Modifier = Modifier
+) {
+    val viewModel: AmsterdamViewModel = viewModel()
+    val amsterdamUiState: AmsterdamUiState = viewModel.uiState.collectAsState().value
+    HomeScreen(
+        amsterdamUiState = amsterdamUiState,
+        onTabPressed = { placeType: PlaceType ->
+            viewModel.updateCurrentPlaceType(placeType = placeType)
+        },
+        onPlaceCardPressed = { place: Place ->
+            viewModel.updatePlaceScreenStates(
+                place = place
+            )
+        },
+        onPlaceTypeScreenBackPressed = {
+            viewModel.resetHomeScreenStates()
+        },
+        onPlaceScreenBackPressed = {
+            viewModel.resetPlaceTypeScreenStates()
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/amsterdam/ui/HomeScreen.kt
+++ app/src/main/java/com/example/amsterdam/ui/HomeScreen.kt
@@ -2,14 +2,27 @@ package com.example.amsterdam.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,28 +33,113 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.amsterdam.R
+import com.example.amsterdam.data.Place
+import com.example.amsterdam.data.PlaceType
+import com.example.amsterdam.data.ScreenType
 import com.example.compose.AmsterdamTheme
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
-    Column (
+fun HomeScreen(
+    amsterdamUiState: AmsterdamUiState,
+    onTabPressed: (PlaceType) -> Unit,
+    onPlaceCardPressed: (Place) -> Unit,
+    onPlaceTypeScreenBackPressed: () -> Unit,
+    onPlaceScreenBackPressed: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
         modifier = modifier
-    ){
-        Header(
-            stringResource(R.string.app_name)
-        )
+            .fillMaxSize()
+    ) {
+        if (amsterdamUiState.currentScreen == ScreenType.Home) {
+            Header(stringResource(R.string.app_name))
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(PlaceType.entries) { category ->
+                    CategoryCard(category, onClick = onTabPressed )
+                }
+            }
+
+        } else if (amsterdamUiState.currentScreen == ScreenType.PlaceType) {
+            PlaceTypeContents(
+                amsterdamUiState = amsterdamUiState,
+                onTabPressed = onTabPressed,
+                onPlaceCardPressed = onPlaceCardPressed,
+                onPlaceTypeScreenBackPressed = onPlaceTypeScreenBackPressed,
+                modifier = Modifier
+            )
+        } else {
+            PlaceContents(
+                amsterdamUiState = amsterdamUiState,
+                onPlaceScreenBackPressed = onPlaceScreenBackPressed,
+                modifier = Modifier
+            )
+        }
     }
 }
 
 @Composable
-fun Header(title: String, modifier: Modifier = Modifier) {
+fun CategoryCard(
+    category: PlaceType,
+    onClick: (category: PlaceType) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+            .clickable { onClick(category) },
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = category.imageRes),
+                contentDescription = category.label,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.matchParentSize()
+            )
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(Color.Black.copy(alpha = 0.4f))
+            )
+            Text(
+                text = category.name,
+                color = Color.White,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(8.dp)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun Header(
+    title: String,
+    modifier: Modifier = Modifier,
+    isFullScreen: Boolean = false,
+    onBackButtonClicked: () -> Unit = {}
+) {
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(100.dp)
+            .height(120.dp)
     ) {
         Image(
             painter = painterResource(id = R.drawable.wallpaper),
@@ -55,20 +153,43 @@ fun Header(title: String, modifier: Modifier = Modifier) {
                 .background(
                     brush = Brush.verticalGradient(
                         colors = listOf(
-                            Color.White.copy(alpha = 0.9f),
+                            Color.White.copy(alpha = 0.6f),
                             Color.Transparent
                         )
                     )
                 )
         )
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
                 .padding(16.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
+            contentAlignment = Alignment.Center
         ) {
-            Text(title, style = MaterialTheme.typography.titleLarge)
+            if (isFullScreen) {
+                IconButton(
+                    onClick = onBackButtonClicked,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .background(
+                            MaterialTheme.colorScheme.surface.copy(alpha = 0.7f),
+                            shape = CircleShape
+                        )
+                        .align(Alignment.CenterStart),
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.navigation_back),
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            }
+            Text(
+                title,
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.scrim,
+                textAlign = TextAlign.Center
+            )
+
         }
     }
 }
@@ -76,7 +197,13 @@ fun Header(title: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun HomeScreenPreview() {
-    AmsterdamTheme {
-        HomeScreen()
+    AmsterdamTheme(dynamicColor = false) {
+        HomeScreen(
+            amsterdamUiState = AmsterdamUiState(),
+            onTabPressed = {},
+            onPlaceCardPressed = {},
+            onPlaceTypeScreenBackPressed = {},
+            onPlaceScreenBackPressed = {}
+        )
     }
 }
