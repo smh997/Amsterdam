6cef736a8234cd7de472006649860e28e5781035
smh10 <smh10.mh@gmail.com>
2025-02-27 15:57:52 -0500

    WindowStateUtils.kt including NavigationType is implemented to support different window sizes. Also some dependencies are added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,18 +41,23 @@ android {
 
 dependencies {
 
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.text.google.fonts)
-
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.material3)
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.androidx.material3.window.size)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
